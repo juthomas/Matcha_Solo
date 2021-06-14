5e334cc0dc639915cc0edc97edebc2b471ce2ea7
juthomas <juthomas@student.42.fr>
2021-06-14 17:13:26 +0200

    [ADD] Back end work without localhost

--- client/src/components/SwipeCard/SwipeCard.js
+++ client/src/components/SwipeCard/SwipeCard.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import "./SwipeCard.css"
 import $ from "jquery"
 import Axios from "axios"
-import reactElementToJSXString from 'react-element-to-jsx-string';
+// import reactElementToJSXString from 'react-element-to-jsx-string';
 
 function SwipeCard() {
 
@@ -13,7 +13,7 @@ function SwipeCard() {
 	var dbOffset = useRef(0);
 	const dbLimit = 5;
 	const [clicksNumber, setClicksNumber] = useState(0);
-	var items = []
+	// var items = []
 	const [itemsState, setItemsState] = useState([]);
 
 
@@ -35,23 +35,22 @@ function SwipeCard() {
 	// console.log(items);
 
 
+	// useEffect((getNewProfiles), []);
 
-	useEffect(() => {
-		getNewProfiles()
-	}, [])
 
-	function refreshProfilesItems() {
+
+	const refreshProfilesItems = useCallback(() => {
 		console.log("beg reload");
 		console.log("reload datas", profilesDatas.current);
 	
 		console.log("profiles data :",profilesDatas.current);
-		items = profilesDatas.current.map((val, key) => 
+		var items = profilesDatas.current.map((val, key) => 
 		{
 			return (
 				<div key={key} className="demo__card">
 				<div className="demo__card__top brown">
 				<div className="demo__card__img">
-					<img style={{height:"100%", }} src="https://thispersondoesnotexist.com/image"></img>
+					<img style={{height:"100%", }} src="https://thispersondoesnotexist.com/image" alt="Profile_image"></img>
 				</div>
 				<p className="demo__card__name">Boy {val.name}</p>
 				</div>
@@ -75,12 +74,17 @@ function SwipeCard() {
 		
 		console.log("reload");
 		setItemsState(items);
-	}
+	}, [])
 
-	function getNewProfiles(){
-		Axios.post("http://localhost:3001/get_profiles", 
+	const getNewProfiles = useCallback(() => {
+		// Axios.post("http://localhost:3001/get_profiles", 
+		// TODO: Replace with a correct thing:
+		var urlPrefix = window.location.protocol + "//" + window.location.hostname + ":3001";
+		console.log("Host :" + urlPrefix);
+		Axios.post(urlPrefix + "/get_profiles", 
 		{offset: dbOffset.current, limit: dbLimit})
 		.then((response) => {console.log(response)
+
 		if (dbLimit > response.data.length)
 		{
 			dbOffset.current = 0;
@@ -100,7 +104,11 @@ function SwipeCard() {
 		setNumberOfProfiles(profilesDatas.current.length)
 		refreshProfilesItems()
 		});
-	}
+	}, [refreshProfilesItems]);
+
+	useEffect(() => {
+		getNewProfiles()
+	}, [getNewProfiles]);
 
 	// useEffect(() => {
 
@@ -235,7 +243,7 @@ function SwipeCard() {
 			console.log("exit :", numberOfProfiles)
 			$(document).off();
 		}
-	}, [numberOfProfiles])
+	}, [numberOfProfiles, getNewProfiles])
 	return (
 		<>
 		<div className="SwipeCard">
