0f51c2634548ea4175aa3da9e3bac121a4e0ccfb
luciehdr <houdeyerlucie@hotmail.fr>
2021-06-24 05:25:14 +0200

    Messenger in progress

--- client/src/pages/Messages/Messages.js
+++ client/src/pages/Messages/Messages.js
@@ -4,91 +4,59 @@ import Axios from 'axios'
 
 function Messages() {
     const [relationList, setRelationList] = useState([]);
+    const [messageList, setMessageList] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
 
+    function handleChange(currentMessage) {
+        setNewMessage(currentMessage);
+      }
 
-    // document.querySelector('.chat[data-chat=person2]').classList.add('active-chat');
-    // document.querySelector('.person[data-chat=person2]').classList.add('active');
-    
-    // let friends = {
-    //   list: document.querySelector('ul.people'),
-    //   all: document.querySelectorAll('.left .person'),
-    //   name: '' },
-    
-    // chat = {
-    //   container: document.querySelector('.container .right'),
-    //   current: null,
-    //   person: null,
-    //   name: document.querySelector('.container .right .top .name') };
-    
-    
-    // friends.all.forEach(f => {
-    //   f.addEventListener('mousedown', () => {
-    //     f.classList.contains('active') || setAciveChat(f);
-    //   });
-    // });
-    
-    // function setAciveChat(f) {
-    //   friends.list.querySelector('.active').classList.remove('active');
-    //   f.classList.add('active');
-    //   chat.current = chat.container.querySelector('.active-chat');
-    //   chat.person = f.getAttribute('data-chat');
-    //   chat.current.classList.remove('active-chat');
-    //   chat.container.querySelector('[data-chat="' + chat.person + '"]').classList.add('active-chat');
-    //   friends.name = f.querySelector('.name').innerText;
-    //   chat.name.innerHTML = friends.name;
-    // }
 
     useEffect(() => {
             var urlPrefix = window.location.protocol + "//" + window.location.hostname + ":3001";
 
-            Axios.post(urlPrefix + "/user/get_relationships", { userId: 253 })
+            Axios.post(urlPrefix + "/user/get_previews", { userId: 292 })
                 .then((response) => {
                         console.log(response.data);
 
                         var tmpRelationList = response.data.map((item, key) => {
                                 return (
                                 // <p key = { key } > { item.id }, { item.name }, { item.old } </p>)
-                                <li className="person" data-chat="person1">
+                                <div className="person" data-chat="person1">
                                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/382994/thomas.jpg" alt="" />
                                 <span className="name">{item.name}</span>
-                                <span className="time">????</span>
-                                <span className="preview"></span>
-                                </li>)
-                                
+                                <span className="time">{item.timestamp}</span>
+                                <span className="preview">{item.message}</span>
+                                </div>)
                                 ;
                                 }); setRelationList(tmpRelationList);
-
                         });
-                }, [])
+                        console.log(messageList);
+                }
+                , [])
                 return (
                         <div className="wrapper">
                         <div className="container">
                             <div className="left">
                                 <div className="top">
                                     <input type="text" placeholder="Search" />
-                                    <a href="javascript:;" className="search"></a>
                                 </div>
-                                <ul className="people">
+                                <div className="people">
                                     {relationList}
-                                </ul>
+                                </div>
                             </div>
                             <div className="right">
                                 <div className="top"><span>To: <span className="name">Dog Woofson</span></span></div>
-    
+                                <div className="chatWrap">
                                 <div className="chat active-chat" data-chat="person4">
-
-                                    <div className="conversation-start">
-                                        <span>Yesterday, 4:10 PM</span>
-                                    </div>
-                                    <div className="bubble me">
+                                {messageList}
+                                    {/* <div className="bubble me">
                                         Hey human!
                                     </div>
                                     <div className="bubble me">
                                         Umm... Someone took a shit in the hallway.
                                     </div>
-                                    <div className="conversation-start">
-                                        <span>Yesterday, 4:20 PM</span>
-                                    </div>
+                                
                                     <div className="bubble me">
                                         Hey human!
                                     </div>
@@ -104,24 +72,40 @@ function Messages() {
                                     <div className="bubble me">
                                         I mean...
                                     </div>
-                                    <div className="conversation-start">
-                                        <span>Yesterday, 4:30 PM</span>
-                                    </div>
+                                
                                     <div className="bubble me">
                                         It’s not that bad...
-                                    </div>
+                                    </div> */}
+                                </div>
                                 </div>
                               
                                 <div className="write">
-                                    <a href="javascript:;" className="write-link attach"></a>
-                                    <input type="text" />
-                                    <a href="javascript:;" className="write-link smiley"></a>
+                                    <input type="text" value={newMessage} onChange={event => setNewMessage(event.target.value)}
+                                        onKeyPress={event => {
+                                            if (event.key === 'Enter') {
+                                                var urlPrefix = window.location.protocol + "//" + window.location.hostname + ":3001";
+                                                Axios.post(urlPrefix + "/user/send_message", { userId: 292, friendId: 254, message:{newMessage} }).then((response) => {
+                                                    setNewMessage("");
+                                                    Axios.post(urlPrefix + "/user/get_messages", { userId: 292, friendId:254 })
+                                                    .then((response) => {
+                                                            console.log(response.data);
+                                    
+                                                            var tmpMessageList = response.data.map((item, key) => {
+                                                                    return (
+                                                                    <div className={item.id == 292 ? "bubble me" : "bubble you"}>
+                                                                        {item.message}
+                                                                    </div>
+                                                                    );
+                                                                    }); setMessageList(tmpMessageList);
+                                                            });
+                                                });
+                                            }
+                                     }} />
                                     <a href="javascript:;" className="write-link send"></a>
                                 </div>
                             </div>
                         </div>
-                    </div>
-
+                        </div>
         )
     }
 
--- server/routes/User.js
+++ server/routes/User.js
@@ -74,12 +74,70 @@ router.post("/get_relationships", (req, res) => {
 						latitude: result.latitude,
 						longitude: result.latitude
 					});
-
 				});
 				console.log(relationshipsData);
 				res.json(relationshipsData);
 			});
 		});
+})
+
+router.post("/get_previews", (req, res) =>
+{
+	const userId = req.body.userId;
+	var previewDatas = [];
+	var relationshipsIds = [];
+	var currentName = "";
+	db.query("SELECT a.id, a.Message, a.sender, a.receiver, a.friend_id FROM Messages a LEFT JOIN Messages b ON (a.friend_id = b.friend_id AND a.id < b.id) WHERE b.id is null AND (a.sender = 1 OR a.receiver = 1) ORDER BY a.id DESC	", [userId, userId, userId],
+	(err, results) => {
+		if (err) {
+			console.log(err);
+		}
+		if (results.length) {
+		
+		}
+	})
+}
+);
+
+router.post("/get_messages", (req, res) => {
+	console.log("get messages");
+	const userId = req.body.userId;
+	const friendId = req.body.friendId;
+	var messageData = [];
+
+	db.query("SELECT  * FROM messages WHERE   (receiver_id = ? AND sender_id = ?) OR (sender_id = ? AND receiver_id = ?) ORDER BY time_stamp", [userId, friendId, userId, friendId],
+		(err, results) => {
+			if (err) {
+				console.log(err);
+			}
+			if (results.length) {
+				results.forEach((elem) => {
+					messageData.push({
+						id: elem.sender_id,
+						message: elem.Message,
+						name: elem.receiver_name
+					});
+				})
+			}
+				console.log(messageData);
+				res.json(messageData);
+			});
+})
+
+router.post("/send_message", (req, res) => {
+	console.log("send message");
+	const userId = req.body.userId;
+	const friendId = req.body.friendId;
+	const message = req.body.message.newMessage;
+	const name = req.body.name;
+	console.log(message);
+	db.query("INSERT INTO Messages (sender_id, receiver_id, Message) VALUES (?, ?, ?);",
+			[userId, friendId, message, name],
+		(err, results) => {
+			if (err) {
+				console.log(err);
+			}
+			});
 })
 
 
